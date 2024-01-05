8c5bccba23313ea9ff97dea3df781f03218e4e81
Kim Chanho <beroso20th@gmail.com>
2024-01-05 15:36:25 +0900

    feature : computed property names ES6

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -26,34 +26,69 @@ const ListPage = () => {
         {id: 4, title: "제목4", content: "내용4"},
         {id: 5, title: "제목5", content: "내용5"},
     ]);
+
     const handleWrite = () => {
-        // ListPage의 setPosts에 무엇을 담아야 함?
-        let post = {id: 6, title: "인풋값"};
+        setPosts([...posts, post]);
     }
+    console.log(posts);
 
-    const handleChangeTitle = (e) => {
-        console.log(e.target.value);
-        setPost({title:e.target.value});
-    }
+    // const handleChangeTitle = (e) => {
+    //     console.log(e.target.value);
+    //     setPost({title:e.target.value});
+    // }
+    // const handleChangeContent = (e) => {
+    //     console.log(e.target.value);
+    //     setPost({content:e.target.value});
+    // }
 
-    const handleChangeContent = (e) => {
-        console.log(e.target.value);
-        setPost({content:e.target.value});
+    const handleForm = (e) => {
+        // ES6 computed property name : 키 동적 할당
+        setPost({...post, [e.target.name] : e.target.value});
+
+        // let val1 = "Heeseung Chae";
+        // console.log("my name is " + val1);  // my name is Heeseung Chae
+        //
+        // let val2 = "Chanho Kim";
+        // console.log(`my name is ${val2}`); // my name is Chanho Kim
+        //
+        // let val3 = "Donghwan Kim";
+        // let obj1 = {
+        //     name : `${val3}`,
+        //     name2 : val2
+        // }
+        // console.log(obj1);
+        //
+        //
+        // let key1 = "name";
+        // let key2 = "age";
+        // let age1 = 30;
+        // let obj2 = {
+        //     // error
+        //     // `${key1}` : val1;
+        //     [key1] : "Heeseung Chae",
+        //     [key1] : val2,
+        //     [key2] : age1
+        // }
+        // console.log(obj2);
+
     }
 
     return (
         <div>
             <h1>리스트 페이지</h1>
             <form>
-                <input type="text" placeholder="제목을 입력하세요..." value={post.title} onChange={handleChangeTitle}/>
-                <input type="text" placeholder="내용을 입력하세요..." value={post.content} onChange={handleChangeContent}/>
+                <input type="text" placeholder="번호를 입력하세요..." value={post.id} onChange={handleForm} name="id"/>
+                {/*<input type="text" placeholder="제목을 입력하세요..." value={post.title} onChange={handleChangeTitle}/>*/}
+                <input type="text" placeholder="제목을 입력하세요..." value={post.title} onChange={handleForm} name="title"/>
+                {/*<input type="text" placeholder="내용을 입력하세요..." value={post.content} onChange={handleChangeContent}/>*/}
+                <input type="text" placeholder="내용을 입력하세요..." value={post.content} onChange={handleForm} name="content"/>
                 <button type="button" onClick={handleWrite}>글쓰기</button>
             </form>
             <hr/>
             {posts.map((post) =>
                 <StyledItemBoxDiv>
                     <div>
-                        번호:{post.id} 제목:{post.title} 내용: {post.content}
+                        번호: {post.id} / 제목: {post.title} / 내용: {post.content}
                     </div>
                     <button>삭제</button>
                 </StyledItemBoxDiv>
